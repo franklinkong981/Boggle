b798424ec830bedfd55b3a0b5f12497812737a25
Franklin Kong <franklinkong981@gmail.com>
2024-01-11 15:24:00 -0800

    Added tests for Boggle class

--- tests/test_boggle.py
+++ tests/test_boggle.py
@@ -3,4 +3,20 @@ from boggle import Boggle
 
 class BoggleTestCase(TestCase):
     """Test methods/test cases for the Boggle class and its methods in boggle.py"""
+    def testInit(self):
+        """Test to see whether the Boggle object is properly initialized with the right attributes."""
+        boggle_game = Boggle()
+        self.assertIsInstance(boggle_game.words, list)
+        self.assertIsInstance(boggle_game.words[0], str)
+
+    def testMakeBoard(self):
+        """Test to see whether the board returned from the makeBoard function in the Boggle class consists of the right dimensions and 
+        contents"""
+        boggle_game = Boggle()
+        board = boggle_game.make_board()
+        self.assertEqual(len(board), 5)
+        self.assertEqual(len(board[0]), 5)
+        self.assertTrue(board[0][0].isalpha())
+        self.assertTrue(board[0][0].isupper())
+
 
