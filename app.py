8c2010ff0a6fc4e6548d67bf7a045e730eaa9275
Franklin Kong <franklinkong981@gmail.com>
2024-01-11 14:41:15 -0800

    Added base html and css files, created route to display board and form to submit words

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, redirect, flash, jsonify, session, make_response
 from flask_debugtoolbar import DebugToolbarExtension
+from boggle import Boggle
 
 
 app = Flask(__name__)
@@ -7,4 +8,13 @@ app.config['SECRET_KEY'] = "oh-so-secret"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
+# Initialize the Boggle game
+boggle_game = Boggle()
+
+@app.route('/game')
+def start_game():
+    """Generate the 5x5 board of letters, store it into the session under the key "board", and display the board on the page
+    as a table with a form to submit words the user sees on the board."""
+    session["board"] = boggle_game.make_board()
+    return render_template("game.html")
 
